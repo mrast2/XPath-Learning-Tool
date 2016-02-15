ddb1af8b216b1ef18df096c82324115da0cf3fbc
mrast2 <mayank.rastogi88@gmail.com>
2016-02-15 17:39:51 -0500

    Learn XPath
    This tool dynamically generates most of the possible XPaths for a
    selected XML file using Recursion and shows corresponding XML nodes for
    selected XPath.

---
+++ XmlAssignment/XmlAssignment/LearnXPath.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace XmlAssignment
+{
+    partial class LearnXPath
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtXmlPath = new System.Windows.Forms.TextBox();
+            this.btn_showXML = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.cmb_xpaths = new System.Windows.Forms.ComboBox();
+            this.txt_selectedNode = new System.Windows.Forms.TextBox();
+            this.txt_xPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_showResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(420, 23);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_browse.TabIndex = 0;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtXmlPath
+            // 
+            this.txtXmlPath.Location = new System.Drawing.Point(43, 25);
+            this.txtXmlPath.Name = "txtXmlPath";
+            this.txtXmlPath.ReadOnly = true;
+            this.txtXmlPath.Size = new System.Drawing.Size(354, 20);
+            this.txtXmlPath.TabIndex = 1;
+            // 
+            // btn_showXML
+            // 
+            this.btn_showXML.Location = new System.Drawing.Point(177, 69);
+            this.btn_showXML.Name = "btn_showXML";
+            this.btn_showXML.Size = new System.Drawing.Size(75, 23);
+            this.btn_showXML.TabIndex = 3;
+            this.btn_showXML.Text = "Show XML";
+            this.btn_showXML.UseVisualStyleBackColor = true;
+            this.btn_showXML.Click += new System.EventHandler(this.btn_showXML_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.webBrowser1.Location = new System.Drawing.Point(43, 117);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(452, 408);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // cmb_xpaths
+            // 
+            this.cmb_xpaths.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmb_xpaths.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmb_xpaths.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmb_xpaths.FormattingEnabled = true;
+            this.cmb_xpaths.Location = new System.Drawing.Point(568, 71);
+            this.cmb_xpaths.Name = "cmb_xpaths";
+            this.cmb_xpaths.Size = new System.Drawing.Size(523, 21);
+            this.cmb_xpaths.TabIndex = 5;
+            this.cmb_xpaths.SelectedValueChanged += new System.EventHandler(this.cmb_xpaths_SelectedValueChanged);
+            // 
+            // txt_selectedNode
+            // 
+            this.txt_selectedNode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_selectedNode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_selectedNode.Location = new System.Drawing.Point(568, 117);
+            this.txt_selectedNode.Multiline = true;
+            this.txt_selectedNode.Name = "txt_selectedNode";
+            this.txt_selectedNode.ReadOnly = true;
+            this.txt_selectedNode.Size = new System.Drawing.Size(523, 408);
+            this.txt_selectedNode.TabIndex = 6;
+            // 
+            // txt_xPath
+            // 
+            this.txt_xPath.Location = new System.Drawing.Point(661, 26);
+            this.txt_xPath.Name = "txt_xPath";
+            this.txt_xPath.ReadOnly = true;
+            this.txt_xPath.Size = new System.Drawing.Size(217, 20);
+            this.txt_xPath.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter xPath";
+            // 
+            // btn_showResult
+            // 
+            this.btn_showResult.Location = new System.Drawing.Point(910, 24);
+            this.btn_showResult.Name = "btn_showResult";
+            this.btn_showResult.Size = new System.Drawing.Size(75, 23);
+            this.btn_showResult.TabIndex = 9;
+            this.btn_showResult.Text = "Show XML";
+            this.btn_showResult.UseVisualStyleBackColor = true;
+            this.btn_showResult.Click += new System.EventHandler(this.btn_showResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 558);
+            this.Controls.Add(this.btn_showResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_xPath);
+            this.Controls.Add(this.txt_selectedNode);
+            this.Controls.Add(this.cmb_xpaths);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btn_showXML);
+            this.Controls.Add(this.txtXmlPath);
+            this.Controls.Add(this.btn_browse);
+            this.Name = "Form1";
+            this.Text = "Understanding xPaths";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtXmlPath;
+        private System.Windows.Forms.Button btn_showXML;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ComboBox cmb_xpaths;
+        private System.Windows.Forms.TextBox txt_selectedNode;
+        private System.Windows.Forms.TextBox txt_xPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_showResult;
+    }
+}
+
---
+++ XmlAssignment/XmlAssignment/LearnXPath.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using System.IO;
+using System.Xml.Linq;
+
+namespace XmlAssignment
+{
+    public partial class LearnXPath : Form
+    {
+        /// <summary>
+        /// XmlDocument object to load the xml
+        /// </summary>
+        XmlDocument xDoc = new XmlDocument();
+
+        public LearnXPath()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the click event of Browse XML button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Open XML Document";
+            openFileDialog1.Filter = "XML File |*.xml";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                txtXmlPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Handles the click event of Show XML button
+        /// and displays the xml to the user on the Win form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_showXML_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(txtXmlPath.Text))
+            {
+                try
+                { 
+                    xDoc.Load(txtXmlPath.Text);
+                    webBrowser1.Url = new Uri(xDoc.BaseURI);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message + " Please validate the selected file!", "Warning!",MessageBoxButtons.OKCancel, MessageBoxIcon.Warning,MessageBoxDefaultButton.Button1);                    
+                }
+                try
+                {
+                    XmlElement root = xDoc.DocumentElement;
+                    cmb_xpaths.Text = "--Select xPath--";
+                    if (root.HasAttributes)
+                    {
+                        for (int i = 0; i < root.Attributes.Count; i++)
+                        {
+                            cmb_xpaths.Items.Add("/" + root.Name + "[@" + root.Attributes[i].Name + "='" + root.Attributes[i].Value + "']");
+                        }
+                    }
+                    cmb_xpaths.Items.Add("/" + root.Name);
+                    var childElements = root.ChildNodes;
+                    foreach (XmlNode childNode in childElements)
+                    {
+                        if (((XmlElement)childNode).HasAttributes)
+                        {
+                            for (int i = 0; i < ((XmlElement)childNode).Attributes.Count; i++)
+                                cmb_xpaths.Items.Add("/" + root.Name + "/" + childNode.Name + "[@" + childNode.Attributes[i].Name + "='" + ((XmlElement)childNode).Attributes[i].Value + "']");
+                        }
+                        if(childNode.HasChildNodes)
+                        {
+                            for (int i = 0; i < childNode.ChildNodes.Count; i++)
+                                cmb_xpaths.Items.Add("/" + root.Name + "/" + childNode.Name + "/" + childNode.ChildNodes[i].Name);
+                        }
+                    }
+                    var allElements = xDoc.SelectNodes("//*");
+                    foreach (XmlNode Node in allElements)
+                    {
+                        string xPath = GetXPath(Node);
+                        cmb_xpaths.Items.Add(xPath);
+                    }
+
+                    List<object> list = new List<object>();
+                    foreach (object obj in cmb_xpaths.Items)
+                    {
+                        if (!list.Contains(obj))
+                        {
+                            list.Add(obj);
+                        }
+                    }
+                    cmb_xpaths.Items.Clear();
+                    cmb_xpaths.Items.AddRange(list.ToArray());
+                    txt_xPath.ReadOnly = false;
+                }
+                catch(NullReferenceException ex)
+                {
+                }
+            }
+            else
+            {
+                txtXmlPath.Text = "Please select a file!";
+            }
+            
+        }
+
+        /// <summary>
+        /// handles the event for change in value of the dropdownlist.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmb_xpaths_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {             
+                    XmlNodeList resultList = xDoc.SelectNodes(cmb_xpaths.Text);
+                    string nodeList = string.Empty;
+                    foreach (XmlNode xNode in resultList)
+                    {
+                        string temp = FormatXmlString(xNode.OuterXml.Trim());
+                        temp = temp.Substring(temp.IndexOf('>') + 1);
+                        nodeList += temp;
+                    }
+                    txt_selectedNode.Text = nodeList.Trim();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An unexpected error occured while querying the xml!" + ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Formats the xml in a user friendly way.
+        /// </summary>
+        /// <param name="xml">xml in string format</param>
+        /// <returns></returns>
+        static public string FormatXmlString(string xml)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "\t",
+                NewLineChars = Environment.NewLine,
+                NewLineHandling = NewLineHandling.Replace,
+            };
+
+            using (var ms = new MemoryStream())
+            using (var writer = XmlWriter.Create(ms, settings))
+            {
+                doc.Save(writer);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// returns the xPath for the node selected by the user
+        /// </summary>
+        /// <param name="node">Node to be displayed</param>
+        /// <returns>xPath</returns>
+        private string GetXPath(XmlNode node)
+        {
+            string temp = null;
+            XmlNode sibling = default(XmlNode);
+            int previousSiblings = 1;
+            if (node.Name == "#document")
+                return "";
+
+            sibling = node.PreviousSibling;
+            while (sibling != null)
+            {
+                if (sibling.Name == node.Name)
+                {
+                    previousSiblings += 1;
+                }
+                sibling = sibling.PreviousSibling;
+            }
+
+            temp = node.Name + (previousSiblings > 0 || node.NextSibling != null ? "[" + previousSiblings.ToString() + "]" : "").ToString();
+
+            if (node.ParentNode != null)
+            {
+                return GetXPath(node.ParentNode) + "/" + temp;
+            }
+
+            return temp;
+        }
+
+        /// <summary>
+        /// Handles the click event for button to show the results 
+        /// of xPath user entered in the textbox provided.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_showResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlNodeList resultList = xDoc.SelectNodes(txt_xPath.Text);
+                string nodeList = string.Empty;
+                foreach (XmlNode xNode in resultList)
+                {
+                    string temp = FormatXmlString(xNode.OuterXml.Trim());
+                    temp = temp.Substring(temp.IndexOf('>') + 1);
+                    nodeList += temp;
+                }
+                txt_selectedNode.Text = nodeList.Trim();
+                if(string.IsNullOrWhiteSpace(txt_selectedNode.Text))
+                {
+                    txt_selectedNode.Text = "Please Select a valid query or learn from the Combobox above!";
+                }
+            }
+            catch (Exception ex)
+            {
+                txt_selectedNode.Text = "Please enter valid node!";
+            }
+
+        }
+
+        //private static string GetXPathToNode(XmlNode node)
+        //{
+        //    if (node.NodeType == XmlNodeType.Attribute)
+        //    {
+        //        // attributes have an OwnerElement, not a ParentNode; also they have
+        //        // to be matched by name, not found by position
+        //        return String.Format(
+        //            "{0}/@{1}",
+        //            GetXPathToNode(((XmlAttribute)node).OwnerElement),
+        //            node.Name
+        //            );
+        //    }
+        //    if (node.ParentNode == null)
+        //    {
+        //        // the only node with no parent is the root node, which has no path
+        //        return "";
+        //    }
+        //    //get the index
+        //    int iIndex = 1;
+        //    XmlNode xnIndex = node;
+        //    while (xnIndex.PreviousSibling != null) { iIndex++; xnIndex = xnIndex.PreviousSibling; }
+        //    // the path to a node is the path to its parent, plus "/node()[n]", where 
+        //    // n is its position among its siblings.
+        //    return String.Format(
+        //        "{0}/node()[{1}]",
+        //        GetXPathToNode(node.ParentNode),
+        //        iIndex
+        //        );
+        //}
+
+
+    }
+}
